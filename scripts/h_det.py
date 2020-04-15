63e01753e3e6457d8fb5ae9e1a4d6dcf38fbea2f
Caio Freitas <caiofreitas@usp.br>
2020-04-15 17:17:20 -0300

    h_sem_ros

--- scripts/h_det.py
+++ scripts/h_det.py
@@ -8,8 +8,6 @@ from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Vector3Stamped
 
 
-
-####################################################################
 class ShapeDetector:
     def __init__(self):
         rospy.init_node("h_detector")
@@ -28,104 +26,9 @@ class ShapeDetector:
         try:
             self.gray = self.bridge.imgmsg_to_cv2(image, "mono8")
             self.detect()
-            # self.gray = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2GRAY)
-            # self.gray = self.cv_image
 
         except CvBridgeError as e:
             print ("CvBridge Error: " + str(e))
-
-    def detect(self):
-        # Capture frame-by-frame
-            blur1 = cv2.GaussianBlur(self.gray, (9,9), 0)
-            blur2 = cv2.GaussianBlur(blur1, (9,9), 0)
-            _, thresh = cv2.threshold(blur2, 120, 255, cv2.THRESH_BINARY)
-            _, cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # kernel = np.array([[0, 0, 255, 255, 0, 0],
-            #                    [0, 0, 255, 255, 0, 0],
-            #                    [0, 0, 0, 0, 0, 0],
-            #                    [0, 0, 0, 0, 0, 0],
-            #                    [0, 0, 255, 255, 0, 0],
-            #                    [0, 0, 255, 255, 0, 0]], np.int32)
-            kernel = np.array([[-1, 5, -1],
-                        [-1, -1, -1],
-                        [-1, 5, -1]], np.float64)
-            kernel2 = np.array([[5, -0.35, -1],
-                                [-0.35, -1, -1],
-                                [-1, -1, 5]], np.float64)
-            kernel3 = np.array([[-1, -1, -1],
-                                [5, -1, 5],
-                                [-1, -1, -1]], np.float64)
-            kernel4 = np.array([[-0.35, -1, 5],
-                                [-1, -1, -1],
-                                [5, -0.35, -1]], np.float64)
-            #cv2.imshow("Convolution kernel", kernel)
-
-            for cnt in cnts:
-                peri = cv2.arcLength(cnt, True)
-                approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-                #cv2.drawContours(self.gray, [approx], 0, (0), 2)
-                x = approx.ravel()[0]
-                y = approx.ravel()[1]
-                
-                #if len(approx) == 3:
-                    #cv2.putText(self.gray, "Triangulo", (x,y), self.font, 1, (0))
-                    #cv2.drawContours(self.gray, [approx], 0, (0), 2)
-                if len(approx) == 12:
-                    cv2.putText(self.gray, "H", (x,y), self.font, 1, (0, 255, 0))
-                    cv2.drawContours(self.gray, [approx], 0, (0, 255, 0), 2)
-
-                    box = np.float32([[0, 0],
-                                    [thresh.shape[0], 0],
-                                    [thresh.shape[0], thresh.shape[1]],
-                                    [0, thresh.shape[1]]])
-                    
-                    self.edge_pts = np.float32([
-                                        [approx[0][0][0], approx[0][0][1]],
-                                        [approx[11][0][0], approx[11][0][1]],
-                                        [approx[5][0][0], approx[5][0][1]],
-                                        [approx[6][0][0], approx[6][0][1]] ])
-                    transformed = self.four_point_transform(thresh, self.edge_pts)
-                    #cv2.imshow("Transformed Image", transformed)
-                    # M = cv2.getPerspectiveTransform(self.edge_pts, box)
-                    # small_img = cv2.warpPerspective(thresh, M, thresh.shape)
-
-                    small_img = cv2.resize(transformed, (3, 3), interpolation=cv2.INTER_AREA)
-                    small_debug_img = self.bridge.cv2_to_imgmsg(small_img, "mono8")
-                    self.small_img_publisher.publish(small_debug_img)
-                    #cv2.imshow("Small Image", small_img)
-                    parts = small_img*kernel
-                    parts2 = small_img*kernel2
-                    parts4 = small_img*kernel4
-                    soma4 = parts4.sum()
-                    soma = parts.sum()
-                    soma2 = parts2.sum()
-                    parts3 = small_img*kernel3
-                    soma3 = parts3.sum()
-                    #print soma
-                    if (soma>=1240 or soma2>=1240 or soma3>=1240 or soma4>=1240):
-                        M = cv2.moments(cnt)
-                        self.detection.vector.x = int(M["m10"] / M["m00"])
-                        self.detection.vector.y = int(M["m01"] / M["m00"])
-                        self.detection.vector.z = cv2.contourArea(cnt)/(self.gray.shape[0]*self.gray.shape[1])
-                        self.detection_pub.publish(self.detection)
-                        cv2.putText(self.gray, "Eh um H", (x,y), self.font, 1, (0, 255, 0))
-                        cv2.drawContours(self.gray, [approx], 0, (0, 255, 0), 2)
-                        cv2.circle(self.gray, (self.detection.vector.x, self.detection.vector.y), 10, (255,255,255),  3)
-                        print(self.detection.vector.z)
-                    # i=0c
-                    # for v in approx:
-                    #     cv2.putText(self.gray, str(i), (v[0][0], v[0][1]), self.font, 1, (0, 255, 0))
-                    #     i += 1
-
-            # Our operations on the self.gray come here
-            #gray = cv2.cvtColor(self.gray, cv2.COLOR_BGR2GRAY)
-
-            # Display the resulting self.gray
-            debug_img = self.bridge.cv2_to_imgmsg(self.gray, "mono8")
-            #print(self.gray.shape)
-            self.img_publisher.publish(debug_img)
-            
-            #cv2.imshow('frame', self.gray)
 
     def order_points(self, pts):
         # initialzie a list of coordinates that will be ordered
@@ -179,6 +82,90 @@ class ShapeDetector:
         warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
         # return the warped image
         return warped
+
+    def detect(self):
+        # Capture frame-by-frame
+            blur1 = cv2.GaussianBlur(self.gray, (9,9), 0)
+            blur2 = cv2.GaussianBlur(blur1, (9,9), 0)
+            _, thresh = cv2.threshold(blur2, 120, 255, cv2.THRESH_BINARY)
+            _, cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            kernel = np.array([[-1, 3.5, -1],
+                                [-1, -1, -1],
+                                [-1, 3.5, -1]], np.float64)
+            kernel2 = np.array([[5, -0.35, -1],
+                                [-0.35, -1, -1],
+                                [-1, -1, 5]], np.float64)
+            kernel3 = np.array([[-1, -1, -1],
+                                [5, -1, 5],
+                                [-1, -1, -1]], np.float64)
+            kernel4 = np.array([[-0.35, -1, 5],
+                                [-1, -1, -1],
+                                [5, -0.35, -1]], np.float64)
+            #cv2.imshow("Convolution kernel", kernel)
+
+            for cnt in cnts:
+                peri = cv2.arcLength(cnt, True)
+                approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+
+                x = approx.ravel()[0]
+                y = approx.ravel()[1]
+                
+                if len(approx) == 12:
+                    cv2.putText(self.gray, "H", (x,y), self.font, 1, (0, 255, 0))
+                    cv2.drawContours(self.gray, [approx], 0, (0, 255, 0), 2)
+
+                    box = np.float32([[0, 0],
+                                    [thresh.shape[0], 0],
+                                    [thresh.shape[0], thresh.shape[1]],
+                                    [0, thresh.shape[1]]])
+                    
+                    self.edge_pts = np.float32([
+                                        [approx[0][0][0], approx[0][0][1]],
+                                        [approx[11][0][0], approx[11][0][1]],
+                                        [approx[5][0][0], approx[5][0][1]],
+                                        [approx[6][0][0], approx[6][0][1]] ])
+                    transformed = self.four_point_transform(thresh, self.edge_pts)
+
+                    small_img = cv2.resize(transformed, (3, 3), interpolation=cv2.INTER_AREA)
+                    small_debug_img = self.bridge.cv2_to_imgmsg(small_img, "mono8")
+                    self.small_img_publisher.publish(small_debug_img)
+                    #cv2.imshow("Small Image", small_img)
+                    parts = small_img*kernel
+                    parts2 = small_img*kernel2
+                    parts4 = small_img*kernel4
+                    soma4 = parts4.sum()
+                    soma = parts.sum()
+                    soma2 = parts2.sum()
+                    parts3 = small_img*kernel3
+                    soma3 = parts3.sum()
+                    
+                    #print soma
+                    if (soma>=1240 or soma2>=1240 or soma3>=1240 or soma4>=1240):
+                        M = cv2.moments(cnt)
+                        self.detection.vector.x = int(M["m10"] / M["m00"])
+                        self.detection.vector.y = int(M["m01"] / M["m00"])
+                        self.detection.vector.z = cv2.contourArea(cnt)/(self.gray.shape[0]*self.gray.shape[1])
+                        self.detection_pub.publish(self.detection)
+                        cv2.putText(self.gray, "Eh um H", (x,y), self.font, 1, (0, 255, 0))
+                        cv2.drawContours(self.gray, [approx], 0, (0, 255, 0), 2)
+                        cv2.circle(self.gray, (self.detection.vector.x, self.detection.vector.y), 10, (255,255,255),  3)
+                        print(self.detection.vector.z)
+                    
+                    # i=0
+                    # for v in approx:
+                    #     cv2.putText(self.gray, str(i), (v[0][0], v[0][1]), self.font, 1, (0, 255, 0))
+                    #     i += 1
+
+            # Our operations on the self.gray come here
+            #gray = cv2.cvtColor(self.gray, cv2.COLOR_BGR2GRAY)
+
+            # Display the resulting self.gray
+            debug_img = self.bridge.cv2_to_imgmsg(self.gray, "mono8")
+            #print(self.gray.shape)
+            self.img_publisher.publish(debug_img)
+            
+            #cv2.imshow('frame', self.gray)
+
 
 ####################################################################
     def run(self):
