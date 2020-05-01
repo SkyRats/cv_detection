0cf5372139321e2105dfc671015b9a03cff016f2
tomaz-suller <tomaz.suller@usp.br>
2020-05-01 08:09:23 -0300

    First commit

---
+++ scripts/h_sem_ros.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+
+#include <bits/stdc++.h>
+
+using namespace cv;
+using namespace std;
+
+#define vp vector<Point>
+
+Mat four_points_transform(Mat image, vp pts){
+    
+}
+
+int main(){
+    return 0;
+}
