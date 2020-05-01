01e219a9e86b3fa04eecd48c3c6147cf84075615
tomaz-suller <tomaz.suller@usp.br>
2020-05-01 12:19:27 -0300

    Add proper H detection

--- scripts/detection.cpp
+++ scripts/detection.cpp
@@ -23,22 +23,23 @@ Mat detect (Mat frame)
     findContours(frame, contour, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 
     /* Usei cv::Mat porque já tem multiplicação de matrizes definida */
-    Mat kernel = (Mat_<double>(3,3) << 
+    Mat kernels[4];
+    kernels[0] = (Mat_<double>(3,3) << 
     -1, 3.5, -1 ,
     -1, -1, -1 ,
     -1, 3.5, -1
     );
-    Mat kernel2 = (Mat_<double>(3,3) <<
+    kernels[1] = (Mat_<double>(3,3) <<
     5, -0.35, -1 ,
     -0.35, -1, -1 ,
     -1, -1, 5
     );
-    Mat kernel3 = (Mat_<double>(3,3) << 
+    kernels[2] = (Mat_<double>(3,3) << 
     -1, -1, -1 ,
     5, -1, 5 ,
     -1, -1, -1
     );
-    Mat kernel4 = (Mat_<double>(3,3) << 
+    kernels[3] = (Mat_<double>(3,3) << 
     -0.35, -1, 5 ,
     -1, -1, -1 ,
     5, -0.35, -1
@@ -54,22 +55,38 @@ Mat detect (Mat frame)
             polylines(frame2, approx, true, Scalar(0,255,0), 5, 8, 0);
             imshow("", frame2);
 
+            /* Desnecessário sem debugging
             Mat box = (Mat_<double>(3,3) <<
             0,0 ,
             frame.rows, 0 ,
             frame.rows, frame.cols ,
             0, frame.cols
             );
+            */
             Mat edge_pts = (Mat_<double>(3,3) <<
             approx[0].x, approx[0].y ,
             approx[11].x, approx[11].y , 
             approx[5].x, approx[5].y ,
             approx[6].x, approx[6].y
             );
+
+            Mat transformed = four_points_transform(frame, edge_pts);
+
+            Mat small_img;
+            resize(transformed, small_img, Size(3,3), (0,0), (0,0), INTER_AREA);
             
+            for(int i = 0; i < 4; i++){
+                double soma = sum(small_img*kernels[i]) [0];
+                if(soma >= 1240){
+                    printf("H detectado");
+                    /* cv::putText(frame, "Eh um H", ); <<-- COMPLETAR 
+                    Alem disso, aqui deve ser frame ou frame2 ?
+                    drawContours(frame, approx, 0, (0,255,0), 2); */ 
+                }
+            }
         }
     }
-
+    /* frame (processado) ou frame2 (original)? */
     return frame;
 }
 
@@ -82,7 +99,7 @@ int main()
     {
         imshow("base", frame);
         imshow("display", detect(frame));
-        waitKey(30);
+        if (waitKey(30) == 27) break;
         video >> frame;
     }
 }
