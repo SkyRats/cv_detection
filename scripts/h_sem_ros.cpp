f44901808b412c021234fdbb2dcbd94b6afbdc58
tomaz-suller <tomaz.suller@usp.br>
2020-05-13 22:21:33 -0300

    Fix type compatibility

--- scripts/h_sem_ros.cpp
+++ scripts/h_sem_ros.cpp
@@ -9,12 +9,13 @@ using namespace cv;
 using namespace std;
 
 #define vp vector<Point>
+#define vpf vector<Point2f>
 
-vp order_points(vp pts);
+vpf order_points(vpf pts);
 
-Mat four_points_transform(Mat image, vp pts){
+Mat four_points_transform(Mat image, vpf pts){
 
-    vp rect = order_points(pts);
+    vpf rect = order_points(pts);
 
     /* Se alguem souber de um jeito mais eficiente de fazer isso,
     me avisa */ 
@@ -23,20 +24,29 @@ Mat four_points_transform(Mat image, vp pts){
     Point br = rect[2];
     Point bl = rect[3];
 
-    double widthA =  sqrt( pow( br.x - bl.x, 2.0) - pow( br.y - bl.y, 2.0 ) );  
-    double widthB =  sqrt( pow( tr.x - tl.x, 2.0) - pow( tr.y - tl.y, 2.0 ) );
+    float widthA =  sqrt( abs ( pow( br.x - bl.x, 2.0) - pow( br.y - bl.y, 2.0 ) ) );  
+    float widthB =  sqrt( abs ( pow( tr.x - tl.x, 2.0) - pow( tr.y - tl.y, 2.0 ) ) );
     /* Precisa de mencao explicita porque existe cv::max */
-    double maxWidth = std::max(widthA, widthB);
+    float maxWidth = std::max(widthA, widthB);
 
-    double heightA =  sqrt( pow( br.x - tr.x, 2.0) - pow( br.y - tr.y, 2.0 ) );  
-    double heightB =  sqrt( pow( tl.x - bl.x, 2.0) - pow( tl.y - bl.y, 2.0 ) );
-    double maxHeight = std::max(heightA, heightB);
+    float heightA =  sqrt( abs ( pow( br.y - tr.y, 2.0 ) - pow( br.x - tr.x, 2.0) ) ); 
+    float heightB =  sqrt( abs ( pow( tl.y - bl.y, 2.0 ) - pow( tl.x - bl.x, 2.0) ) );
 
-    Mat dst = (Mat_<double> (4,2) << 
-        0,0 ,
-        maxWidth-1, 0 , 
-        maxWidth-1, maxHeight-1 ,
-        0, maxHeight-1);
+    float maxHeight = std::max(heightA, heightB);
+
+    /* Mat dst = (Mat_<float> (4,2) << 
+        0.0,0.0 ,
+        maxWidth, 0.0 , 
+        maxWidth, maxHeight ,
+        0.0, maxHeight);
+    */
+
+    vp dst = {
+        Point2f(0.0, 0.0) ,
+        Point2f(maxWidth, 0.0) ,
+        Point2f(maxWidth, maxHeight) ,
+        Point2f(0.0, maxHeight)
+    };
 
     Mat M = getPerspectiveTransform(rect, dst);
     Mat warped; 
@@ -47,5 +57,17 @@ Mat four_points_transform(Mat image, vp pts){
 }
 
 int main(){
+
+    Mat frame;
+    VideoCapture feed(-1);
+
+    while(waitKey(30) != 27){
+        feed >> frame;
+
+        vp pts { Point2f(0.0,0.0), Point2f(0.0,50.0), Point2f(50.0,50.0), Point2f(50.0,0.0) };
+
+        imshow("Transform", four_points_transform(frame, pts));
+    }
+
     return 0;
 }
