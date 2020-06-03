3cb45a871a9013e499d24257fb7d6f570e7d0072
pedro-fuoco <pedrofuoco6@gmail.com>
2020-06-03 20:35:51 -0300

    added a generic version of the motion detecter, should be implemented for specific applications

--- src/webcam_motion.cpp
+++ src/webcam_motion.cpp
@@ -2,10 +2,10 @@
 #include <opencv2/opencv.hpp>
 #include <list>
 
-#define MIN_PIX_VALUE 50
+#define MIN_PIX_VALUE 60
 #define MOTION_THRESHOLD 2000 // no quarto do @pedro-fuoco, com todas as luzes acesas
 #define DEBUG true
-#define DILATION_SIZE 7
+#define DILATION_SIZE 20 //mexer aqui para mudar a precisão (solução suficiente por enquanto, talvez precise mudar para aplicaões especificas)
 using namespace std;
 using namespace cv;
 
@@ -18,17 +18,23 @@ class MotionDetecter {
         void image_treat();
         void bounding_recs();
         bool is_relevant(vector<Point> rect);
+
     public:
         MotionDetecter();
+        void frame_reset(Mat frame);
         bool detect(Mat new_image); // ver se é a primera, se for a primeira retornar false
         vector<Rect> relevant_rectangles;
 };
 
+void MotionDetecter::frame_reset(Mat frame){
+    cvtColor(frame, this->old_gray, CV_RGB2GRAY);
+};
 MotionDetecter::MotionDetecter(){
     this->primeira = true;
 };
 
-bool MotionDetecter::detect(Mat new_image) {
+bool MotionDetecter::detect(Mat frame) {
+    Mat new_image = frame;
     if (this->primeira == true) {
         cvtColor(new_image, this->old_gray, CV_RGB2GRAY);
         this->primeira = false;
@@ -38,12 +44,13 @@ bool MotionDetecter::detect(Mat new_image) {
         cvtColor(new_image, this->new_gray, CV_RGB2GRAY);
         image_treat();
         bounding_recs();
-        if(this->total_sum != 0) {
+        //if(this->total_sum != 0) {
+        if(DEBUG){
             if (DEBUG) {
                 for(int i = 0; i < relevant_rectangles.size(); i++){
                     rectangle(new_image, relevant_rectangles[i], Scalar(0,255,0),3,8,0);
                 }
-                putText(new_image, "movement detected", Point(275,25),  FONT_HERSHEY_SIMPLEX, 1, Scalar(0,255,0), 3, 8, false);
+                //putText(new_image, "movement detected", Point(275,25),  FONT_HERSHEY_SIMPLEX, 1, Scalar(0,255,0), 3, 8, false);
                 imshow("Display", new_image);
             }
             //return true;
@@ -53,13 +60,14 @@ bool MotionDetecter::detect(Mat new_image) {
 
 
 bool MotionDetecter::is_relevant(vector<Point> rect) {
-    if(contourArea(rect) > 1000){ //precisamos definir como veremos a area minima
+    if(contourArea(rect) > 10000){ //precisamos definir como veremos a area minima
         return true;
     }
 };
 
 void MotionDetecter::image_treat() {
     Mat subtracted_frame = this->old_gray - this->new_gray;
+    imshow("new", old_gray);
     threshold(subtracted_frame, subtracted_frame, MIN_PIX_VALUE , 255 , 0 );
     dilate(subtracted_frame, subtracted_frame, getStructuringElement(MORPH_RECT, Size(1.25*DILATION_SIZE,1.25*DILATION_SIZE), Point(DILATION_SIZE, DILATION_SIZE)));
     Canny(subtracted_frame, subtracted_frame, 75, 225, 3);
@@ -74,7 +82,6 @@ void MotionDetecter::bounding_recs() {
     findContours(this->cv_frame, stored_contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE); 
     // aqui achamos os contornos que existem na imagem, eles são armazenados em stored_contours
     this->total_sum = 0;
-    // existe um possível bug que o findContours detecta os cantos da tela, pode ser resolvido com int i = 1
     for(int i = 0; i < stored_contours.size(); i++){ 
         approxPolyDP(stored_contours[i], poly, 20, true); //transforma o contorno detectado em um poligono poly
         if (is_relevant(poly)){
@@ -85,39 +92,17 @@ void MotionDetecter::bounding_recs() {
 };
 
 
-
-/* movarea = boundingRect(result);
-
-        if(2*movarea.area() >= (result.cols*result.rows)){
-            
-            Size size = result.size();
-
-            resize(result, result, Size(100, 100), 2, 2);
-            Rect checkarea = boundingRect(result);
-            thresh = 100;
-
-        }else checkarea = movarea;
-    
-        x0 = checkarea.x;
-        y0 = checkarea.y;
-
-        w = checkarea.width + x0;
-        h = checkarea.height + y0;
-        
-        if(sum(result)[0]/255 > thresh){
-            std::cout << "MOVING\n";
-            rectangle(frame1, movarea, Scalar(255, 0, 0), 2);
-        }else std::cout << std::endl;   */
-
-
 int main() {
-    Mat frame;
+    Mat frame, reseter;
     VideoCapture video(-1); // captures video from default cam
 
     MotionDetecter* detecter = new MotionDetecter;
+    int i = 0;
     while (true) {
         video >> frame;
+        video >> reseter;
         detecter->detect(frame);
         waitKey(30);
+        detecter->frame_reset(reseter);
     }   
 }
